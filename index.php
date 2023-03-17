845784907bdcfcaae39624ab304b8cb1393e3a9e
MeblIkea <salulespotos@gmail.com>
2023-03-17 22:07:33 +0100

    a

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Quizz!</title>
+</head>
+<body>
+<div class="title_div">
+    <h1 class="title">Quizz</h1>
+    <label>
+        <input type="text" maxlength="32" placeholder="votre nom" id="name_input">
+        <button class="start_button" onclick="start()">↩</button>
+    </label>
+</div>
+
+<script src="script_main.js"></script>
+</body>
+</html>
